add08be51a5bc946bf76275f1a9b389c6e12667b
v0ncent <83373495+v0ncent@users.noreply.github.com>
2023-12-05 14:24:49 -0600

    made config a record

--- Vscript/src/main/java/Config.java
+++ Vscript/src/main/java/Config.java
@@ -1,14 +1,9 @@
-public class Config {
-    private final String version;
-    private final String auth;
+public record Config(String version, String auth) {
 
-    public Config(String version, String auth) {
-        this.version = version;
-        this.auth = auth;
-    }
     public String getAuth() {
         return auth;
     }
+
     public String getVersion() {
         return version;
     }
