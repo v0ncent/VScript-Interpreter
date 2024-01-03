8c3afae7dbfe2aab7591412b6892848abd53c664
v0ncent <83373495+v0ncent@users.noreply.github.com>
2024-01-03 13:14:29 -0600

    removed random comment

--- Vscript-Interpreter/src/main/java/DataTypes/VscriptString.java
+++ Vscript-Interpreter/src/main/java/DataTypes/VscriptString.java
@@ -1,5 +1,7 @@
 package DataTypes;
 
+import lombok.NonNull;
+
 public final class VscriptString extends VscriptObject<String> {
     public VscriptString(String value) {
         super(value);
@@ -14,7 +16,7 @@ public final class VscriptString extends VscriptObject<String> {
         return this.value.substring(0, 1).toUpperCase() + this.value.substring(1);
     }
 
-    public String center(int length, char character) {
+    public String center(@NonNull int length,@NonNull char character) {
         int lengthOfTrailingCharacters = Math.abs(this.value.length() - length) / 2;
         StringBuilder builder = new StringBuilder();
 
@@ -26,4 +28,33 @@ public final class VscriptString extends VscriptObject<String> {
 
         return builder.toString();
     }
+
+//    public int Count(@NonNull String value, Integer from, Integer to) {
+//        if (!this.value.contains(value)) {
+//            return 0;
+//        }
+//
+//        int count = 0;
+//        int start;
+//        int end;
+//        String[] characters = this.value.split("");
+//        if (from != null && to != null) {
+//            start = from;
+//            end = to;
+//        } else {
+//            start = 0;
+//            end = characters.length;
+//        }
+//
+//        if (start < 0 || end > characters.length) {
+//            throw new IndexOutOfBoundsException();
+//        }
+//
+//        for (int i = start; i < end; i++) {
+//
+//
+//
+//        }
+//
+//    }
 }
